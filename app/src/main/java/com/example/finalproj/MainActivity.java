6ad1898e9e3453bc2241eb82e1523a0ccf3f0355
krjae <crad17@naver.com>
2022-05-15 15:42:25 +0900

    commit

--- app/src/main/java/com/example/finalproj/MainActivity.java
+++ app/src/main/java/com/example/finalproj/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.finalproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button createQRBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createQRBtn = (Button) findViewById(R.id.createQR);
+
+        createQRBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, CreateQR.class);
+                startActivity(intent);
+            }
+        });
     }
 }
